35e304c0a171e8145944741d380dc81804474bee
Aimene Nouri <nouri.aimene65@gmail.com>
2021-03-07 13:31:36 +0100

    udpate

--- Classe_Compte/Compte.cs
+++ Classe_Compte/Compte.cs
@@ -6,5 +6,56 @@ namespace Classe_Compte
 {
     class Compte
     {
+        private int numero;
+        private double solde;
+        private Client Proprietaire;
+        private static int nbr_comptes = 0;
+
+        public Compte(double solde, Client Proprietaire)
+        {
+            nbr_comptes++;
+            numero = nbr_comptes;
+            this.solde = solde;
+            this.Proprietaire = Proprietaire;
+        }
+        public void Crediter(double somme)
+        {
+            solde += somme;
+            Console.Out.WriteLine("Opération bien effectuée");
+        }
+        public void Crediter(Compte C, double somme)
+        {
+            if(C.solde >= somme)
+            {
+                C.solde -= somme;
+                solde += somme;
+                Console.Out.WriteLine("Opération bien effectuée");
+            }
+            else
+               Console.Out.WriteLine("Solde du compte est insuff");
+        }
+        public void Debiter(double somme)
+        {
+            solde -= somme;
+            Console.Out.WriteLine("Opération bien effectuée");
+        }
+        public void Debiter(Compte C, double somme)
+        {
+            if (solde >= somme)
+            {
+                solde -= somme;
+                C.solde += somme;
+                Console.Out.WriteLine("Opération bien effectuée");
+            }
+            else
+                Console.Out.WriteLine("Votre solde est insuff");
+        }
+        public void afficher()
+        {
+            Console.Out.WriteLine("Numéro de Compte: " + numero);
+            Console.Out.WriteLine("Solde de compte: " + solde);
+            Console.Out.WriteLine("Propriétaire du compte est : ");
+                Proprietaire.afficher();
+        }
     }
 }
--- Classe_Compte/Program.cs
+++ Classe_Compte/Program.cs
@@ -6,7 +6,29 @@ namespace Classe_Compte
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Compte c1 = new Compte(1800, new Client("Nouri", "Aimene", "CASABLANCA"));
+            c1.afficher();
+
+            Compte c2 = new Compte(180000, new Client("Nouri", "Hassan", "CASABLANCA"));
+            c2.afficher();
+
+            //Crediter un compte
+            Console.Out.WriteLine("\nCrediter le Compte");
+            c1.Crediter(c2, 1600);
+
+            Console.Out.WriteLine("\nApres le credit: ");
+            c1.afficher();
+            c2.afficher();
+
+            //Debiter un compte
+            Console.Out.WriteLine("\nDebiter le Compte");
+            c1.Debiter(c2, 1600);
+
+            Console.Out.WriteLine("\nApres le debit: ");
+            c1.afficher();
+            c2.afficher();
+
+            Console.ReadKey();
         }
     }
 }
